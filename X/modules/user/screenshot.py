3db2c638b4ada5be902b01090c8b43fea084e606
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <156512147+nobitaaxd@users.noreply.github.com>
2024-03-23 13:08:02 +0530

    Update screenshot.py from pyrogram import Client, filters client

--- X/modules/user/screenshot.py
+++ X/modules/user/screenshot.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.raw import functions
 from pyrogram.types import Message
 
@@ -12,10 +12,10 @@ from .help import *
 @Client.on_message(
     filters.command(["screenshot", "ss"], ".") & filters.private & filters.me
 )
-async def screenshot(bot: Client, message: Message):
+async def screenshot(client: Client, message: Message):
     await asyncio.gather(
         message.delete(),
-        bot.invoke(
+        client.invoke(
             functions.messages.SendScreenshotNotification(
                 peer=await X.resolve_peer(message.chat.id),
                 reply_to_msg_id=0,
